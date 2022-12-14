c94a682a7e6e5fd4fa3aa2193142736abe7961b5
Diego <diego.gasco99@gmail.com>
2022-12-14 15:17:08 +0100

    Add Errors for parsing and fix some bugs

--- frontend/src/lib/InitialState.svelte
+++ frontend/src/lib/InitialState.svelte
@@ -18,7 +18,6 @@
 
     const start = async () => {
         try {
-            console.log(params.filter);
             let filter = await invoke("new_filter", {});
             let bpf_filters = "";
             if (params.BPF === true) {
@@ -37,74 +36,80 @@
                             case "IPgeneric":
                                 let list_ip = v.split(",");
                                 for (let addr in list_ip) {
+                                    // TODO: a regex to check if the IP address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("add_host", {fil: filter, host: v, not: not})
+                                    filter = await invoke("add_host", {fil: filter, host: addr, not: not})
                                 }
                                 break;
                             case "IPsource":
                                 let list_ips = v.split(",");
                                 for (let addr in list_ips) {
+                                    // TODO: a regex to check if the IP address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("add_src_host", {fil: filter, src_host: v, not: not})
+                                    filter = await invoke("add_src_host", {fil: filter, src_host: addr, not: not})
                                 }
                                 break;
                             case "IPdestination":
                                 let list_ipd = v.split(",");
                                 for (let addr in list_ipd) {
+                                    // TODO: a regex to check if the IP address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("add_dst_host", {fil: filter, dst_host: v, not: not})
+                                    filter = await invoke("add_dst_host", {fil: filter, dst_host: addr, not: not})
                                 }
                                 break;
                             case "MACgeneric":
                                 let list_mac = v.split(",");
                                 for (let addr in list_mac) {
+                                    // TODO: a regex to check if the MAC address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("set_ether_host", {fil: filter, ether_host: v, not: not})
+                                    filter = await invoke("set_ether_host", {fil: filter, ether_host: addr, not: not})
                                 }
                                 break;
                             case "MACsource":
                                 let list_macs = v.split(",");
                                 for (let addr in list_macs) {
+                                    // TODO: a regex to check if the MAC address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("set_ether_src_host", {fil: filter, ether_src_host: v, not: not})
+                                    filter = await invoke("set_ether_src_host", {fil: filter, ether_src_host: addr, not: not})
                                 }
                                 break;
                             case "MACdestination":
                                 let list_macd = v.split(",");
                                 for (let addr in list_macd) {
+                                    // TODO: a regex to check if the MAC address is in a correct form
                                     let not = false;
                                     if (addr.includes("[not]") === true) {
                                         not = true;
                                         addr.replace("[not]", "");
                                     }
-                                    filter = await invoke("set_ether_dst_host", {fil: filter, ether_dst_host: v, not: not});
+                                    filter = await invoke("set_ether_dst_host", {fil: filter, ether_dst_host: addr, not: not});
                                 }
                                 break;
                             case "Portgeneric":
                                 let list_port = v.split(",");
-                                for (let addr in list_port) {
+                                for (let port in list_port) {
                                     try {
-                                        filter = await invoke("set_port", {fil: filter, port: parseInt(v)});
+                                        filter = await invoke("set_port", {fil: filter, port: parseInt(port)});
                                     }
                                     catch(e) {
                                         alert(e);            
@@ -113,20 +118,28 @@
                                 break;
                             case "Portsource":
                                 let list_ports = v.split(",");
-                                for (let addr in list_ports) {
-                                    filter = await invoke("set_src_port", {fil: filter, src_port: parseInt(v)});
+                                for (let port in list_ports) {
+                                    filter = await invoke("set_src_port", {fil: filter, src_port: parseInt(port)});
                                 }
                                 break;
                             case "Portdestination":
                                 let list_portd = v.split(",");
-                                for (let addr in list_portd) {
-                                    filter = await invoke("set_dst_port", {fil: filter, dst_port: parseInt(v)});
+                                for (let port in list_portd) {
+                                    filter = await invoke("set_dst_port", {fil: filter, dst_port: parseInt(port)});
                                 }
                                 break;
                             case "Protocols":
                                 let list_proto = v.split(",");
-                                filter = await invoke("set_protocols", {fil: filter, proto: list_proto});
+                                try {
+                                    filter = await invoke("set_protocols", {fil: filter, proto: list_proto});
+                                }
+                                catch {
+                                    throw new Error("Protocol not found");
+                                }
                                 break;
+                            default:
+                                throw new Error("Incorrect field!");
+                                
                         }
                     }
                     bpf_filters = await invoke("get_BPF_filter", {fil: filter});
